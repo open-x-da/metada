e89baac8177fea3332ce3f9e27d04a5a07f23379
open-x-da <open.x.da@gmail.com>
2025-01-28 17:10:22 +0900

    Refactor CMake project configuration and initialization
    - Simplify project setup by moving version and language configuration to project() command
    - Replace metada_project_settings() with metada_project_initialize()
    - Streamline build environment configuration
    - Set default build type to Release if not specified

--- cmake/ProjectSetup.cmake
+++ cmake/ProjectSetup.cmake
@@ -1,43 +1,20 @@
-# Function to configure project settings
-function(metada_project_settings name)
-    cmake_parse_arguments(ARG
-        ""
-        "VERSION"
-        "LANGUAGES"
-        ${ARGN}
-    )
-
-    # Set default languages if not specified
-    if(NOT ARG_LANGUAGES)
-        set(ARG_LANGUAGES C CXX)
-    endif()
-
-    # Set default version if not specified
-    if(NOT ARG_VERSION)
-        set(ARG_VERSION 0.1.0)
-    endif()
-
+# Function to initialize project build environment
+function(metada_project_initialize)
     # Forbid in-source builds
     if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
         message(FATAL_ERROR "In-source builds are not allowed. Please create a 'build' directory and run CMake from there.")
     endif()
 
-    # Update project settings
-    project(${name}
-        VERSION ${ARG_VERSION}
-        LANGUAGES ${ARG_LANGUAGES}
-    )
+    # Set build type if not specified
+    if(NOT CMAKE_BUILD_TYPE)
+        set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Build type" FORCE)
+    endif()
 
     # Include compiler flags configuration
     include(CompilerFlags)
 
     # Include code formatting configuration
     include(CodeFormat)
-
-    # Set build type specific flags
-    if(NOT CMAKE_BUILD_TYPE)
-        set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Build type" FORCE)
-    endif()
 
     # Include package configuration module
     include(package/Config)
