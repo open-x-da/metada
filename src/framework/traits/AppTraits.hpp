3de1a4b4e099e70a1c2b55349e8c21f4a0bccde0
open-x-da <open.x.da@gmail.com>
2025-02-27 17:30:46 +0800

    Enhance AppTraits with variadic template support
    - Add generic AppTraits template to support multiple backend configurations
    - Implement specialization for Logger and Config backends
    - Provide flexibility for future backend extensions
    - Maintain existing trait type definitions

--- src/framework/traits/AppTraits.hpp
+++ src/framework/traits/AppTraits.hpp
@@ -1,6 +1,14 @@
 // Unified Traits Class that combines all dependencies
+template <typename... Backends>
+struct AppTraits {};
+
+// Specialization for Logger and Config backends
 template <typename LoggerBackend, typename ConfigBackend>
-struct AppTraits {
+struct AppTraits<LoggerBackend, ConfigBackend> {
     using LoggerType = LoggerBackend;            // Logger backend
     using ConfigType = ConfigBackend;            // Config backend
 };
+
+// Future specializations can be added as needed:
+// template <typename LoggerBackend, typename ConfigBackend, typename NewBackend>
+// struct AppTraits<LoggerBackend, ConfigBackend, NewBackend> { ... };
