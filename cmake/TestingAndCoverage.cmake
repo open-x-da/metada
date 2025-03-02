75c3786af6deee38b6dafedcb8b60938269285c3
open-x-da <open.x.da@gmail.com>
2025-03-02 08:27:41 +0800

    Add pre-test coverage data cleaning and minor documentation update
    - Implement pre-build hook in CMake to clean .gcda coverage files before running tests
    - Add custom CMake script to remove coverage data files
    - Minor documentation formatting improvements in NonCopyable header
    - Enhance test coverage data management for more accurate reporting

--- cmake/TestingAndCoverage.cmake
+++ cmake/TestingAndCoverage.cmake
@@ -8,6 +8,14 @@ macro(metada_add_test_with_coverage target)
       GTest::gtest_main
       GTest::gmock)
 
+  # Add a pre-test hook to clean .gcda files before running this test
+  add_custom_command(
+    TARGET ${target}
+    PRE_BUILD
+    COMMAND ${CMAKE_COMMAND} -DCMAKE_BINARY_DIR=${CMAKE_BINARY_DIR} -P ${CMAKE_SOURCE_DIR}/cmake/scripts/clean_gcda.cmake
+    COMMENT "Cleaning coverage data before building ${target}"
+  )
+
   gtest_discover_tests(${target} -D TEST_DISCOVERY_TIMEOUT=60)
 
   #AddMemcheck(${target})
--- src/framework/common/utils/NonCopyable.hpp
+++ src/framework/common/utils/NonCopyable.hpp
@@ -4,18 +4,18 @@ namespace metada::framework {
 
 /**
  * @brief Utility class to make derived classes non-copyable
- * 
+ *
  * Classes can inherit from this to prevent copying while still allowing
  * move semantics. This is cleaner than explicitly deleting copy operations
  * in each class.
- * 
+ *
  * Example usage:
  * @code
  * class MyResource : public NonCopyable {
  *   // This class is now non-copyable but still movable
  * };
  * @endcode
- * 
+ *
  * This is similar to boost::noncopyable but doesn't require Boost.
  */
 class NonCopyable {
@@ -23,14 +23,14 @@ class NonCopyable {
   // Allow construction and destruction
   NonCopyable() = default;
   ~NonCopyable() = default;
-  
+
   // Prevent copying
   NonCopyable(const NonCopyable&) = delete;
   NonCopyable& operator=(const NonCopyable&) = delete;
-  
+
   // Allow moving
   NonCopyable(NonCopyable&&) = default;
   NonCopyable& operator=(NonCopyable&&) = default;
 };
 
-}  // namespace metada::framework 
+}  // namespace metada::framework
